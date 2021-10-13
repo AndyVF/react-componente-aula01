fe8a115db9d087b2b8141bd6f0436af771205ec7
Anderson <andersonfinho@hotmail.com>
2021-10-13 17:49:40 -0300

    componentes react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Mensagem from "./Mensagem"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá Mundoo</h1>
+      <Mensagem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Mensagem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Mensagem() {
+    return(
+        <Mensagem>
+            <p>Volta teu rosto sempre na direção do sol</p>
+            <p>e então, as sombras ficarão para trás.</p>
+        </Mensagem>
+    )
+    
+    
+
+    
+}
